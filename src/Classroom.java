dac4715272dbf087cf00e3d4d8e7c10279dc8e6e
ESCARMANT Adrien <92037454+E-Adrien8@users.noreply.github.com>
2022-10-24 21:50:18 +0200

    finish

---
+++ src/Classroom.java
@@ -0,0 +1,11 @@
+public class Classroom {
+    public static void main(String[] args) {
+
+        Wilder jc = new Wilder("Jean-claude", true);
+        System.out.println(jc.whoAmI());
+
+        Wilder hr = new Wilder("Henri", false);
+        System.out.println(hr.whoAmI());
+
+    }
+}
